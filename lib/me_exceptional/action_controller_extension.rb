76d0f8e059fedbdb64948e34f4b8728870e1976c
Kai Rubarth <krubar@gmail.com>
2013-03-14 15:11:29 +0000

    adding html linebreak to backtrace

--- lib/me_exceptional/action_controller_extension.rb
+++ lib/me_exceptional/action_controller_extension.rb
@@ -10,13 +10,12 @@ module ActionController
        if Rails.env == 'production' 
         # blocking to be sure this gets send!
         MeExceptional::Mailer.exceptional(e.message,
-                            :backtrace => e.backtrace.join("\n"),
+                            :backtrace => e.backtrace.join("<br />"),
                             :timestamp => Time.zone.now,
                             :method => request.method,
                             :url => request.url,
                             :params => params.inspect.to_s,
                             :user_agent => request.user_agent,
-                            #:client_ip => request.remote_ip,
                             :client_ip => request.env["HTTP_X_FORWARDED_FOR"].split(',').first,
                             :request_env => request.env.inspect.to_s).deliver
 
